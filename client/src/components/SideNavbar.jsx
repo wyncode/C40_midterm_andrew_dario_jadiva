83b5cdc5ae9c7f69b77e9ac3c4471c29b28f8778
JadivaM <jmont261@fiu.edu>
2020-11-24 15:19:50 -0500

    edited search results from react bootstrap for recipe list

--- client/src/components/SideNavbar.jsx
+++ client/src/components/SideNavbar.jsx
@@ -9,30 +9,34 @@ import SideNav, {
 
 // Be sure to include styles at some point, probably during your bootstraping
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
 const SideNavbar = () => {
   return (
     <SideNav
+      className="stylingnav"
       style={{ background: 'blue' }}
-      onSelect={(selected) => {
-        // Add your code here
-      }}
+      onSelect={(selected) => {}}
     >
       <SideNav.Toggle />
       <SideNav.Nav defaultSelected="home">
         <NavItem eventKey="home">
-          <NavIcon>
-            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+          <NavIcon style={{ color: 'white' }}>
+            <i
+              className="fa fa-fw fa-home"
+              style={{ fontSize: '1.75em', color: 'white' }}
+            />
           </NavIcon>
-          <NavText>Home</NavText>
+          <NavText style={{ color: 'white' }}>Home</NavText>
         </NavItem>
         <NavItem eventKey="charts">
           <NavIcon>
             <i
               className="fa fa-fw fa-line-chart"
               style={{ fontSize: '1.75em' }}
+              style={{ color: 'white' }}
             />
           </NavIcon>
-          <NavText>Charts</NavText>
+          <NavText style={{ color: 'white' }}>About</NavText>
           <NavItem eventKey="charts/linechart">
             <NavText>Line Chart</NavText>
           </NavItem>
@@ -46,3 +50,25 @@ const SideNavbar = () => {
 };
 
 export default SideNavbar;
+
+// sample
+// export const SideNavbarData = {
+//   title: 'Home',
+//   path: '/',
+//   className: 'nav-item'
+// },
+// {
+//   title: 'Categories',
+//   path: '/',
+//   className: 'nav-item'
+// },
+// {
+// title: 'Featured',
+// path: '/',
+// className: 'nav-item'
+// },
+// {
+// title: 'Vegan/Vegetarian',
+// path: '/',
+// className: 'nav-item'
+// },
